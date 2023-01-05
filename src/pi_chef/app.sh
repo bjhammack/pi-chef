b800676f3adf72e143ace5f9bc07bce8c6d0913a
Benjamin Hammack <bjhammack@protonmail.com>
2023-01-04 22:54:38 -0600

    Updated css and fixed bugs in app.sh.

--- src/pi_chef/app.sh
+++ src/pi_chef/app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source ../../../venvs/pi-chef/bin/activate
+source ../../../../venvs/pi-chef/bin/activate
 
 flask --app pi_chef_webapp run &
 
-chromium-broswer --kiosk http://127.0.0.1:5000
+chromium-browser --kiosk http://127.0.0.1:5000
